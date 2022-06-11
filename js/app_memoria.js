ec0c247f50ebaadbe14a02b13103018cea03d75f
Diego Sampaio <diego.estaleiro@gmail.com>
2022-06-11 20:52:35 -0300

    Lógica do jogo da Memoria

---
+++ js/app_memoria.js
@@ -0,0 +1,67 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+
+  console.log('aqui')
+  console.log(firstCard.dataset.objeto)
+  console.log(secondCard.dataset.objeto)
+
+  if (firstCard.dataset.objeto === secondCard.dataset.objeto) {
+    disableCards();
+    return;
+  }
+
+  unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500)
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  })
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
